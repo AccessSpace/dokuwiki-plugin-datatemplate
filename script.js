c86ecc236c61dae78c422ccb10c5684bff6db9f6
Martyn Eggleton <martyn.eggleton@gmail.com>
2013-05-16 10:46:50 +0100

    Switching to arrowless boxes and dynamic positioning

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 /* DOKUWIKI:include jquery.tools.min.js */
 
-
 jQuery(document).ready(function(){
-    jQuery(".day").tooltip({
- 
-   // tweak the position
-   offset: [10,0],
-   relative:true,
-   predelay:100,
- 
-   // use the "slide" effect
-   effect: 'slide'
- 
-// add dynamic plugin with optional configuration for bottom edge
-});
+  jQuery(".day").tooltip({
+    relative:true,
+    predelay:100,
+    //offset: [10,0],
+    // use the "slide" effect
+    effect: 'slide'
+  }).dynamic();
+  
+  // TODO : get other classes
+  jQuery(".day").one("onBeforeShow", function(event) {
+    var sID = jQuery(this).next().find('.cal-ppecdg-btn').prop('id');
+    var dg = new PAYPAL.apps.DGFlow({
+      trigger: sID,
+      expType: 'instant'
+    });
+  });
+  
 });
